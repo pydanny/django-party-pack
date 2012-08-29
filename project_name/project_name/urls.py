b52ef2b0947e79b13531d53fc54c76acb8c475c9
Daniel Greenfeld <pydanny@gmail.com>
2012-08-29 16:19:19 -0700

    gogo

--- project_name/project_name/urls.py
+++ project_name/project_name/urls.py
@@ -1,15 +1,21 @@
 from django.conf import settings
 from django.conf.urls import patterns, include, url
 from django.contrib import admin
+from django.views.generic import TemplateView
 
 admin.autodiscover()
 
 urlpatterns = patterns('',
 
+    url(regex=r'^$',
+        view=TemplateView.as_view(template_name="home.html"),
+        name="home"
+        ),
     url(r'^admin/', include(admin.site.urls)),
     url(r'^accounts/', include('registration.backends.default.urls')),
+    url(r'^accounts/', include('registration.backends.default.urls')),
     url(r'^login/$', 'django.contrib.auth.views.login', {'template_name': 'registration/login.html'}, name="login"),
-    url(r'^logout/$', 'django.contrib.auth.views.logout_then_login', {'login_url': '/login/'}, 'logout',),    
+    url(r'^logout/$', 'django.contrib.auth.views.logout_then_login', {'login_url': '/login/'}, 'logout',),
 )
 
 
